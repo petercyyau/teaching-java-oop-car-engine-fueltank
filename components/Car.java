cca51487f6049ebc3ac388489e1806e6ff469c78
Peter YAU <9554866+petercyyau@users.noreply.github.com>
2024-01-29 16:11:10 +0800

    Update Car.java

--- components/Car.java
+++ components/Car.java
@@ -1,7 +1,6 @@
-package components;
 /*
  * Author: Peter Yau
- * Email: PeterCY.Yau@glasgow.ac.uk
+ * E-mail: PeterCY.Yau@glasgow.ac.uk
  * Date: 20240122
  * 
  * Description: The Car class represents a vehicle with an engine and a fuel tank.
