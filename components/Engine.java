3f8e8bf9838542964dba62fcafed6f0e4c3754c8
Peter YAU <9554866+petercyyau@users.noreply.github.com>
2024-01-29 16:11:39 +0800

    Update Engine.java

--- components/Engine.java
+++ components/Engine.java
@@ -1,7 +1,6 @@
-package components;
 /*
  * Author: Peter Yau
- * Email: PeterCY.Yau@glasgow.ac.uk
+ * E-mail: PeterCY.Yau@glasgow.ac.uk
  * Date: 20240122
  * 
  * Description: The Engine class represents an internal combustion engine in a vehicle.
